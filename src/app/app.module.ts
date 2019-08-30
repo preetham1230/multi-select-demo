21f56966a9711991518c5c3e895d16fd62fa4367
preethamreddy <tharun.reddy4u@transcore.ca>
2019-08-30 14:34:02 -0400

    Multi select demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { FormsModule } from '@angular/forms';
+import { TextBoxComponent } from './text-box/text-box.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TextBoxComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgSelectModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
